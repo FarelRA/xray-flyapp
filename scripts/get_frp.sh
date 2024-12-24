88c093af8a5b7f460ed958e958ea683e9c028708
Farel Reski Aditiya <farel@farelra.my.id>
2024-12-24 17:13:19 +0700

    fix 'get_frp.sh'

--- scripts/get_frp.sh
+++ scripts/get_frp.sh
@@ -6,8 +6,8 @@ REPO="frp"
 API_URL="https://api.github.com/repos/$OWNER/$REPO/releases/latest"
 
 # Function to extract the tarball URL from the JSON response
-extract_tarball_url() {
-  echo "$1" | grep '"tarball_url":' | sed -E 's/.*"([^"]+)".*/\1/'
+Fetch_tarball_url() {
+   curl -s "$1" | grep '"tarball_url":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
 
 # Function to download and extract the tarball
@@ -15,19 +15,9 @@ download_and_extract_tarball() {
   curl -s "$1" | tar -xz --strip-components 1
 }
 
-# Fetch the latest release information
-echo "Fetching the latest release information..."
-json_response=$(curl -s "$API_URL")
-
-# Check if the response is empty
-if [ -z "$json_response" ]; then
-  echo "Error: Failed to fetch release information from GitHub API."
-  exit 1
-fi
-
-# Extract the tarball URL
-echo "Extracting the tarball URL..."
-tarball_url=$(extract_tarball_url "$json_response")
+# Fetch the latest tarball URL
+echo "Fetching the latest tarball URL..."
+tarball_url=$(fetch_tarball_url "$API_URL")
 
 # Check if a valid tarball URL is found
 if [ -z "$tarball_url" ]; then
